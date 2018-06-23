e4de43dfda1b70890dfe7c411a8760cdcc65693e
EDennis <edennis@wfscorp.com>
2018-06-23 09:59:43 -0400

    fixed mobile issues

--- js/main.js
+++ js/main.js
@@ -1,1 +1,53 @@
-function getFormData(o){var a=o.serializeArray(),t={};return $.map(a,function(o,a){t[o.name]=o.value}),t}$(window).bind("scroll",function(){var o=$(window).scrollTop();o<100?($(".logo").show(),$(".logo").css({opacity:1})):o<200?($(".logo").show(),$(".logo").css({opacity:.4})):$(".logo").hide()}),$("#submitContact").click(function(){console.log(getFormData($("#contact-form"))),$.ajax({url:"http://localhost:8080/contact",type:"POST",data:JSON.stringify(getFormData($("#contact-form"))),contentType:"application/json; charset=utf-8",dataType:"json",success:function(){alert(data)}})}),window.sr=ScrollReveal({reset:!0}),sr.reveal(".logo"),sr.reveal(".plan",{duration:600}),sr.reveal(".platform-card"),sr.reveal("#contact-card");
+$(window).bind('scroll', function () {
+  var scrollTop = $(window).scrollTop();
+  var isVisible = $('#smiling').is(':visible');
+  if (isVisible && scrollTop < 100 ) {
+    $('.smiling').show();
+    $('.smiling').css({
+      opacity: 1
+    });
+  } else if (isVisible && scrollTop < 200 ) {
+    $('.smiling').show();
+    $('.smiling').css({
+      opacity: 0.4
+    });
+  } else {
+    $('.smiling').css({
+      opacity: 0
+    });
+  }
+});
+
+$('#submitContact').click(function () {
+  console.log(getFormData($('#contact-form')));
+
+  $.ajax({
+    url: 'http://localhost:8080/contact',
+    type: 'POST',
+    data: JSON.stringify(getFormData($('#contact-form'))),
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    success: function () {
+      alert(data);
+    }
+  });
+});
+
+function getFormData($form) {
+  var unindexed_array = $form.serializeArray();
+  var indexed_array = {};
+
+  $.map(unindexed_array, function (n, i) {
+    indexed_array[n['name']] = n['value'];
+  });
+
+  return indexed_array;
+}
+
+
+// SCROLL REVEAL CODE
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.logo');
+sr.reveal('.plan', { duration: 600 });
+sr.reveal('.platform-card');
+sr.reveal('#contact-card');
