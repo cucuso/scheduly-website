7196cca279f9bb6106cd0a7242257cc44bc8b001
Eduardo Dennis <edennis@wfscorp.com>
2019-11-25 00:09:32 -0500

    added gif

--- js/main.js
+++ js/main.js
@@ -15,43 +15,9 @@ $(window).bind('scroll', function () {
   }
 });
 
-$('#submitContact').click(function () {
-  console.log(getFormData($('#contact-form')));
-
-  $.ajax({
-    url: 'http://localhost:8080/contact',
-    type: 'POST',
-    data: JSON.stringify(getFormData($('#contact-form'))),
-    contentType: 'application/json; charset=utf-8',
-    dataType: 'json',
-    success: function () {
-      alert(data);
-    }
-  });
-});
-
-function getFormData($form) {
-  var unindexed_array = $form.serializeArray();
-  var indexed_array = {};
-
-  $.map(unindexed_array, function (n, i) {
-    indexed_array[n['name']] = n['value'];
-  });
-
-  return indexed_array;
-}
-
-
 // SCROLL REVEAL CODE
 window.sr = ScrollReveal({ reset: true });
 sr.reveal('.logo');
 sr.reveal('.plan', { duration: 600 });
 sr.reveal('.platform-card');
 sr.reveal('#contact-card');
-
-// INIT GLIDE CAROUSEL
-var glide = new Glide('.glide', {
-  type: 'slider'
-});
-
-glide.mount()
