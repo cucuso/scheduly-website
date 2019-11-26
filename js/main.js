9ba4691f14824611ad4303e8dbd35cbd048dec15
Eduardo Dennis <edennis@wfscorp.com>
2019-11-25 23:39:58 -0500

    added new header and testimonials

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(window).bind('scroll', function () {
   if (scrollTop < 100 ) {
     $('.calendar-img').show();
     $('.calendar-img').css({
-      opacity: 1
+      opacity: .7
     });
   } else if ( scrollTop < 200 ) {
     $('.calendar-img').show();
@@ -17,7 +17,6 @@ $(window).bind('scroll', function () {
 
 // SCROLL REVEAL CODE
 window.sr = ScrollReveal({ reset: true });
-sr.reveal('.logo');
 sr.reveal('.plan', { duration: 600 });
-sr.reveal('.platform-card');
-sr.reveal('#contact-card');
+sr.reveal('.demo-img', { duration: 600 });
+sr.reveal('.busy-img', { duration: 600 });
