b3703bff6b0cdfa1dd6364c47bf590dabf6919f6
Eduardo Dennis <edennis@iralogix.com>
2020-12-06 20:09:51 -0500

    SEO optimizations

--- js/main.js
+++ js/main.js
@@ -1,22 +1,16 @@
 $(window).bind('scroll', function () {
   var scrollTop = $(window).scrollTop();
-  if (scrollTop < 100 ) {
+  if (scrollTop < 100) {
     $('.calendar-img').show();
     $('.calendar-img').css({
-      opacity: .7
+      opacity: 0.7,
     });
-  } else if ( scrollTop < 200 ) {
+  } else if (scrollTop < 200) {
     $('.calendar-img').show();
     $('.calendar-img').css({
-      opacity: 0.4
+      opacity: 0.4,
     });
   } else {
     $('.calendar-img').hide();
   }
 });
-
-// SCROLL REVEAL CODE
-window.sr = ScrollReveal({ reset: true });
-sr.reveal('.plan', { duration: 600 });
-sr.reveal('.demo-img', { duration: 600 });
-sr.reveal('.busy-img', { duration: 600 });
